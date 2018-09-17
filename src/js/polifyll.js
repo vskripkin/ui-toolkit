154d71c12fb08f1177732ea84d4f47d7c3268fc1
Vladimir Skripkin <vladimir-scripkin@ya.ru>
2018-09-17 21:13:27 +0300

    working on visibility mixins

---
+++ src/js/polifyll.js
@@ -0,0 +1,16 @@
+;(function ()
+{
+	if (document.documentElement.hidden === undefined)
+	{
+		Object.defineProperty(Element.prototype, 'hidden', {
+			set: function (_bHidden)
+			{
+				_bHidden ? this.setAttribute('hidden', true) : this.removeAttribute('hidden');
+			},
+			get: function ()
+			{
+				return this.getAttribute('hidden');
+			}
+		});
+	}
+})();
