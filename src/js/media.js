b6ffbdb644a44180d0dcfb2b1d36c52559dae6d5
Vladimir Skripkin <vladimir-scripkin@ya.ru>
2018-10-02 16:46:02 +0300

    update _.media; update bower version

--- src/js/media.js
+++ src/js/media.js
@@ -12,42 +12,63 @@
 	};
 
 	// TODO use window.matchMedia
+	_.media.small_ic = function ()
+	{
+		return window.innerWidth <= this.SMALL_IC;
+	};
+
 	_.media.small = function ()
 	{
-		return window.innerWidth <= this.SMALL;
+		return window.innerWidth < this.MEDIUM;
 	};
+
 	_.media.mediumUp = function ()
 	{
-		return window.innerWidth > this.SMALL;
+		return window.innerWidth >= this.MEDIUM;
+	};
+	_.media.mediumDown = function ()
+	{
+		return window.innerWidth < this.LARGE;
 	};
 	_.media.mediumOnly = function ()
 	{
 		var w = window.innerWidth;
 
-		return w > this.SMALL && w <= this.MEDIUM;
+		return w >= this.MEDIUM && w < this.LARGE;
 	};
+
 	_.media.largeUp = function ()
 	{
-		return window.innerWidth > this.MEDIUM;
+		return window.innerWidth >= this.LARGE;
+	};
+	_.media.largeDown = function ()
+	{
+		return window.innerWidth < this.XLARGE;
 	};
 	_.media.largeOnly = function ()
 	{
 		var w = window.innerWidth;
 
-		return w > this.MEDIUM && w <= this.LARGE;
+		return w >= this.LARGE && w < this.XLARGE;
 	};
+
 	_.media.xlargeUp = function ()
 	{
-		return window.innerWidth > this.LARGE;
+		return window.innerWidth >= this.XLARGE;
+	};
+	_.media.xlargeDown = function ()
+	{
+		return window.innerWidth < this.XXLARGE;
 	};
 	_.media.xlargeOnly = function ()
 	{
 		var w = window.innerWidth;
 
-		return w > this.LARGE && w <= this.XLARGE;
+		return w >= this.XLARGE && w < this.XXLARGE;
 	};
+
 	_.media.xxlarge = function ()
 	{
-		return window.innerWidth > this.XLARGE;
+		return window.innerWidth >= this.XXLARGE;
 	};
 })(window.jQuery, window._);
