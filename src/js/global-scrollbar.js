4d3e53a1460a22a25c086a6feaded1687f5c64a1
Vladimir Skripkin <vladimir-scripkin@ya.ru>
2018-10-21 15:47:29 +0300

    simplify global-scrollbar.js; update bower version

--- src/js/global-scrollbar.js
+++ src/js/global-scrollbar.js
@@ -3,11 +3,10 @@
 {
 	'use strict';
 
-	// TODO пересчитать ширину после изменения масштаба страницы
-
 	_.globalScrollbar = {
 		prop: 'margin',
 		side: 'right',
+		selector: '.js-scrollbar-offset',
 
 		_hidden: false,
 		_width: 0,
@@ -16,8 +15,16 @@
 
 		_num: 0,
 
-		hide: function (_fCallback)
+		_first: true,
+
+		hide: function (_callback)
 		{
+			if (this._first)
+			{
+				this.prop = this.prop ? (this.prop + '-') : '';
+				this._first = false;
+			}
+
 			this._num++;
 
 			if (this._hidden)
@@ -25,46 +32,60 @@
 				return false;
 			}
 
-			this._hidden = true;
 
 			var nHTML = document.documentElement,
-				sProp = this.prop ? (this.prop + '-') : '';
+				iWidth = Math.round(window.innerWidth - nHTML.clientWidth);
 
+			if (iWidth <= 0)
+			{
+				_callback && _callback(0, this.side, this.prop);
+				return;
+			}
+
+
+			this._hidden = true;
+
+			var sProp = this.prop,
+				sSide = this.side;
+
+			this._width = iWidth;
 			this._prevOverflow = nHTML.style.overflow;
-			this._prevMargin = nHTML.style[sProp + this.side];
+			this._prevMargin = nHTML.style[sProp + sSide];
 
 			nHTML.style.overflow = 'hidden';
-			nHTML.style[sProp + this.side] = this._width + 'px';
+			nHTML.style[sProp + sSide] = iWidth + 'px';
 
-			if (_.isFunction(_fCallback))
-			{
-				_fCallback(this._width, this.side, this.prop);
-			}
-			else
-			{
-				var anNeedOffset = document.querySelectorAll('.js-scrollbar-offset');
 
-				_.forEach(anNeedOffset, function (_nElem)
+			var anNeedOffset = document.querySelectorAll(this.selector),
+				nElem, sDataProp,
+				i, L;
+
+			for (i = 0, L = anNeedOffset.length; i < L; i++)
+			{
+				nElem = anNeedOffset[i];
+				sDataProp = nElem.getAttribute('data-offset-prop');
+
+				if (sDataProp === 'true')
 				{
-					var sDataProp = _nElem.getAttribute('data-offset-prop');
+					nElem.style[sSide] = iWidth + 'px';
+				}
+				else if (sDataProp)
+				{
+					nElem.style[sDataProp + '-' + sSide] = iWidth + 'px';
+				}
+				else
+				{
+					nElem.style[sProp + sSide] = iWidth + 'px';
+				}
+			}
 
-					if (sDataProp === 'true')
-					{
-						_nElem.style[this.side] = this._width + 'px';
-					}
-					else if (sDataProp)
-					{
-						_nElem.style[sDataProp + '-' + this.side] = this._width + 'px';
-					}
-					else
-					{
-						_nElem.style[sProp + this.side] = this._width + 'px';
-					}
-				},
-				this);
+
+			if (_callback)
+			{
+				_callback(iWidth, sSide, sProp);
 			}
 		},
-		restore: function (_fCallback, _bRightNow)
+		restore: function (_callback, _bRightNow)
 		{
 			this._num--;
 
@@ -73,131 +94,48 @@
 				return false;
 			}
 
-			var sProp = this.prop ? (this.prop + '-') : '';
 
 			this._hidden = false;
 
-			document.documentElement.style.overflow = this._prevOverflow;
-			document.documentElement.style[sProp + this.side] = this._prevMargin;
+			var sProp = this.prop,
+				sSide = this.side,
+				nHTML = document.documentElement;
+
+			nHTML.style.overflow = this._prevOverflow;
+			nHTML.style[sProp + sSide] = this._prevMargin;
 
 			this._prevOverflow = '';
 			this._prevMargin = '';
 
-			if (_.isFunction(_fCallback))
-			{
-				_fCallback(this._width, this.side, this.prop);
-			}
-			else
-			{
-				var anNeedOffset = document.querySelectorAll('.js-scrollbar-offset');
 
-				_.forEach(anNeedOffset, function (_nElem)
+			var anNeedOffset = document.querySelectorAll(this.selector),
+				nElem, sDataProp,
+				i, L;
+
+			for (i = 0, L = anNeedOffset.length; i < L; i++)
+			{
+				nElem = anNeedOffset[i];
+				sDataProp = nElem.getAttribute('data-offset-prop');
+
+				if (sDataProp === 'true')
 				{
-					var sDataProp = _nElem.getAttribute('data-offset-prop');
-
-					if (sDataProp === 'true')
-					{
-						_nElem.style[this.side] = '';
-					}
-					else if (sDataProp)
-					{
-						_nElem.style[sDataProp + '-' + this.side] = '';
-					}
-					else
-					{
-						_nElem.style[sProp + this.side] = '';
-					}
-				},
-				this);
-			}
-		},
-
-		_set: function (_nFiredElem, _ID)
-		{
-			var that = this;
-
-			setTimeout(function ()
-			{
-				that.set(_nFiredElem, _ID);
-			}, 0);
-		},
-		set: function (_nFiredElem, _ID)
-		{
-			var nWrp =  _nFiredElem.parentElement ||
-						_nFiredElem.parentNode ||
-						document.getElementById(_ID) ||
-						document.querySelector('#' + _ID) ||
-						window[_ID];
-
-			if (!nWrp || !nWrp.offsetWidth)
-			{
-				this._set.apply(this, arguments);
-				return;
+					nElem.style[sSide] = '';
+				}
+				else if (sDataProp)
+				{
+					nElem.style[sDataProp + '-' + sSide] = '';
+				}
+				else
+				{
+					nElem.style[sProp + sSide] = '';
+				}
 			}
 
-			var nInr =  nWrp.firstElementChild ||
-						nWrp.firstChild ||
-						nWrp.children.item(0);
 
-			this._width = nWrp.offsetWidth - nInr.offsetWidth;
-			this.side = document.documentElement.getAttribute('dir') === 'rtl' ? 'left' : 'right';
-
-			if (this._hide)
+			if (_callback)
 			{
-				this._hide(this._hideCallback);
-
-				this.hide = this._hide;
-				this._hide = null;
-				this._hideCallback = null;
+				_callback(this._width, sSide, sProp);
 			}
-
-			if (nWrp.parentElement || nWrp.parentNode)
-			{
-				(nWrp.parentElement || nWrp.parentNode).removeChild(nWrp);
-			}
-			else
-			{
-				nWrp.outerHTML = '';
-			}
-
-			nWrp = nInr = _nFiredElem = null;
 		}
 	};
-
-	(function ()
-	{
-		_.globalScrollbar._hide = _.globalScrollbar.hide;
-
-		_.globalScrollbar.hide = function (_fCallback)
-		{
-			if (_fCallback)
-			{
-				this._hideCallback = _fCallback;
-			}
-
-			var ID = _.randomStr(),
-				STYLE_WRP = 'position: absolute !important;' +
-							'top:     -150px  !important;'  +
-							'left:    -150px  !important;' +
-							'width:    100px  !important;' +
-							'height:   100px  !important;' +
-							'overflow: scroll !important;',
-
-				STYLE_INR = 'display: block !important;' +
-							'width:   100%  !important;' +
-							'height:  20px  !important;',
-
-				nDivWrp = document.createElement('div'),
-				nDivInr = document.createElement('div');
-
-			nDivWrp.id = ID;
-			nDivWrp.setAttribute('style', STYLE_WRP);
-			nDivInr.setAttribute('style', STYLE_INR);
-
-			nDivWrp.appendChild(nDivInr);
-			document.body.appendChild(nDivWrp);
-
-			_.globalScrollbar.set(nDivInr, ID);
-		};
-	})();
 })(window.jQuery, window._);
